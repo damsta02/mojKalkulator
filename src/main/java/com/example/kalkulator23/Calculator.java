c03a1fb829954b7bfa55c62357b9d0d24d0fbd15
damsta02 <116506770+damsta02@users.noreply.github.com>
2023-02-25 10:13:48 +0100

    Update Calculator.java
    promjena damjan

--- src/main/java/com/example/kalkulator23/Calculator.java
+++ src/main/java/com/example/kalkulator23/Calculator.java
@@ -4,6 +4,10 @@ public class Calculator {
     public int saberi(int a, int b){
         return a+b;
     }
+    
+    public int oduzmi(int a, int b){
+        return a-b;
+    }
 
     public static void main(String[] args) {
         Calculator k1=new Calculator();
